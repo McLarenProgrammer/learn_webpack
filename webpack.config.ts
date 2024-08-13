634386af0ca0281dba8d0f29612264f5651002b1
TutnkinMDProg <vip.tutunkin@gmail.com>
2024-08-13 13:29:46 +0500

    Доделал настройку dev-сервера Была добавлена конфигурация для dev.сервера. Установлен live-сервер

--- webpack.config.ts
+++ webpack.config.ts
@@ -36,11 +36,11 @@ export default (env: EnvVariables) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
         },
-        devtool: 'inline-source-map',
-        devServer: {
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? {
             port: env.port ?? 3000,
             open: true,
-        }
+        } : undefined
     }
     return config;
 }
