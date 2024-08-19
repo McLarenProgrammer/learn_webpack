9643d80b63b3a7a9266d02b57dce9bb7e527f7ec
TutnkinMDProg <vip.tutunkin@gmail.com>
2024-08-19 11:33:41 +0500

    Ассеты Настроил работу webpack и typescript с картинками

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,6 +5,34 @@ import { BuildOptions } from "./types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
 
+    const assetLoader = {
+       test: /\.(png|jpg|jpeg|gif)$/i,
+       type: 'asset/resource'
+    }
+
+    const svgrLoader = {
+        test: /\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    currentColor: true,
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        ],
+    }
+
     const cssLoaderWithModules = {
         loader: "css-loader",
         options: {
@@ -24,5 +52,5 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
         exclude: /node_modules/,
     }
 
-    return [scssLoader, tsLoader];
+    return [assetLoader, scssLoader, tsLoader, svgrLoader];
 }
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import classes from './App.module.scss';
 import { Link, Outlet } from "react-router-dom";
 import About from "@/pages/about/About";
+import avatarPng from '@/assets/avatar.png';
+import avatarJpg from '@/assets/avatar.jpg';
+import Avatar from '@/assets/app-image.svg';
 
 export const App = () => {
     const [count, setCount] = useState(0);
@@ -10,6 +13,15 @@ export const App = () => {
 
     return (
         <div>
+            <div>
+                <img width={100} height={100} src={avatarPng} alt="" />
+            </div>
+            <div>
+                <img width={100} height={100} src={avatarJpg} alt="" />
+            </div>
+            <div>
+                <Avatar className={classes.icon} width={50} height={50} />
+            </div>
             <Link to={'/'}>Главная</Link>
             <br />
             <Link to={'/about'}>О нас</Link>
